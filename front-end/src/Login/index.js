f4e25e572363f391efd22025237a918cfc4fa461
Shuyao Xie <shuyaoxi@usc.edu>
2020-11-08 00:38:33 -0500

    login, register tabs changes

--- front-end/src/Login/index.js
+++ front-end/src/Login/index.js
@@ -1,29 +1,38 @@
 import React from "react";
 import FixedContainer from "../FixedContainer";
-import Input from "./Input.js";
+import Input from './Input.js';
+import {Link} from 'react-router-dom';
 
 export default function Login(props) {
   const middle = {
     margin: "auto",
   };
   return (
-    <FixedContainer style={middle}>
-      <div> Log in </div>
-      <form>
+    <FixedContainer >
+      <div style={{display: 'flex',flexDirection:'column',alignItems:'center',height:'65vh',justifyContent:'center'}}>
+      <div style={{ textAlign: 'center', fontSize: '1.5em' }}>Log In</div>
+      <form style={{color:'white',padding:'0px 5px',textAlign:'center'}}>
         <Input
-          placeHolder="username"
-          width="400px"
-          height="50px"
+          placeholder='Username/Email'
           name="username"
         ></Input>
 
         <Input
-          placeHolder="password"
-          width="400px"
-          height="50px"
+          placeholder="Password"
           name="password"
         ></Input>
+        
+        <input type="submit" value="Log In" style={{marginTop:'40px',backgroundColor: '#54BEF5',padding: '5px',border:'none',color:"white",width:'28vw',height:'4vh'}}/>
       </form>
+       <div style={{width:'55vw',display:'flex',justifyContent:'space-between',marginTop:'36px'}}>
+       
+       <Link to='/register' style={{color:'black',textDecoration:'none',fontSize:'3.5vw'}}> Register</Link>
+       <Link to='#' style={{color:'black',textDecoration:'none',fontSize:'3.5vw'}}> Forget Password</Link>
+       
+      </div> 
+      
+      </div>
+      
     </FixedContainer>
   );
 }
