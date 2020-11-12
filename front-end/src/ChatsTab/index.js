1ccc4b2ca9ebb5c1cb1e4372849400d17149fa67
Shuyao Xie <shuyaoxi@usc.edu>
2020-11-12 12:58:44 -0500

    gallery axios except search

--- front-end/src/ChatsTab/index.js
+++ front-end/src/ChatsTab/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import FixedContainer from '../FixedContainer.js';
 import Picture from '../Picture.js';
 import axios from 'axios';
-import getAllUsers from '../api/user';
+ 
 export default function ChatsTab(props){
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'http://localhost:8080/test/getUserList',
+      );
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
 
   return(
     <FixedContainer>
       Chats
       <Picture/>
-      {getAllUsers.getAllUsers()};
+      {/* {list?console.log(list.data):console.log('nothing')} */}
     </FixedContainer>
   );
 }
--- front-end/src/GalleryTab/GalleryCard.js
+++ front-end/src/GalleryTab/GalleryCard.js
@@ -1,64 +1,79 @@
-import React from "react";
-import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
-import { Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
-
-const useStyles = makeStyles({
-  root: {
-    background: "#54BEF5",
-    width: "28vw",
-    fontSize: "0.75em",
-    color: "white",
-    padding: "3px",
-  },
-});
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 export default function GalleryCard(props) {
+  const useStyles = makeStyles({
+    root: {
+      background: '#54BEF5',
+      width: '28vw',
+      fontSize: '0.75em',
+      color: 'white',
+      padding: '3px',
+    },
+  });
+
   const history = useHistory();
   const classes = useStyles();
   const cardStyle = {
-    border: "0.5vw dashed #54BEF5",
-    borderRadius: "12%",
-    width: "35vw",
-    height: "50vw",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: "1.3vw",
-    margin: "3.6vw 4.8vw",
+    border: '0.5vw dashed #54BEF5',
+    borderRadius: '12%',
+    width: '35vw',
+    height: '50vw',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '1.3vw',
+    margin: '3.6vw 4.8vw',
   };
+
 
   const imgStyle = {
-    width: "21vw",
-    height: "21vw",
-    position: "center",
-   
+    width: '21vw',
+    height: '21vw',
+    position: 'center',
   };
 
-  let id='2';
-
-  return (
+  let cardName=props.cardName?props.cardName:'Title';
+  let id=props.id?props.id:'2';
+  let tags=props.tags?props.tags:['#tag1 #tag2 #tag3 #tag4 #tag5 #tag6'];
+  let url=props.url?props.url:'/logo192.png';
   
-    <Link to={`/ActivityCard/${id}`} style={{textDecoration:'none',color:'black'}}>
+  return (
+    <Link
+      to={`/ActivityCard/${id}`}
+      style={{ textDecoration: 'none', color: 'black' }}
+    >
       <div style={cardStyle}>
-        <div className="title" style={{fontSize: '4.3vw'}}>Title</div>
-        <img src="../logo192.png" style={imgStyle}></img>
+        <div className="title" style={{ fontSize: '4.3vw' }}>
+          {cardName}
+        </div>
+        <img src={url} style={imgStyle}></img>
         <div
           className="tags"
           style={{
-            width: "90%",
-            height: "18%",
-            wordWrap: "break-word",
-            margin: "0.5vw 0vw 1.8vw 0vw ",
-            fontSize: "3vw",
+            width: '90%',
+            height: '18%',
+            wordWrap: 'break-word',
+            margin: '0.5vw 0vw 1.8vw 0vw ',
+            fontSize: '3vw',
           }}
         >
-          #tag1#tag1#tag1#tag1
+          {tags.map((tag)=>tag)}
         </div>
-        <Button className={classes.root} style={{maxWidth:'90%'}} onClick={(e)=>{e.preventDefault();
-        history.push('/ToDoList')}}>
-          <span style={{fontSize:'3vw'}}>{props.canAdd ? "Add To My List" : "View In My List"}</span>
+        <Button
+          className={classes.root}
+          style={{ maxWidth: '90%' }}
+          onClick={(e) => {
+            e.preventDefault();
+            history.push('/ToDoList');
+          }}
+        >
+          <span style={{ fontSize: '3vw'}}>
+            {props.canAdd ? 'Add To My List' : 'View In My List'}
+          </span>
         </Button>
       </div>
     </Link>
