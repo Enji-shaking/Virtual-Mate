bd42bd5882d2abca5068d33bbaf188eedded0389
Shuyao Xie <shuyaoxi@usc.edu>
2020-11-14 17:13:57 -0500

    peopleInCard

--- front-end/src/PeopleInCard/index.js
+++ front-end/src/PeopleInCard/index.js
@@ -6,11 +6,10 @@ import LockOpenRoundedIcon from '@material-ui/icons/LockOpenRounded';
 import axios from 'axios';
 
 export default function CardView(props) {
-  let avatars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ 
   let { id } = useParams();
 
-  const [users, setUsers] = useState([
-    { Username: 'tester', Userid: 'testUser', Avatar: '/logo.png' },
+  const [avatars, setUsers] = useState([
   ]);
   useEffect(() => {
     const fetchData = async () => {
@@ -20,11 +19,14 @@ export default function CardView(props) {
     fetchData();
   }, []);
 
-  let userId='testUser';
+ 
+  let user='f3e2a8b4-e95e-45f2-a94e-f88833f07383';
+  let pass = '123456';
+
+
   let done = false;
-  for(let i=0;i<users.length;i++) {
-    console.log(users[i]);
-    if (users[i].Userid === userId) {
+  for(let i=0;i<avatars.length;i++) {
+    if (avatars[i].userId === user) {
       done = true;
       break;
     }
@@ -56,34 +58,19 @@ export default function CardView(props) {
               return (
                 <Link
                   to={`/User/${
-                    avatar.Userid ? avatar.Userid : avatar.toString()
+                    avatar.userId ? avatar.userId : avatar.toString()
                   }`}
-                  key={avatar.Userid ? avatar.Userid : avatar.toString()}
+                  key={avatar.userId ? avatar.userId : avatar.toString()}
                 >
                   <Avatar
-                    alt={avatar.toString()}
-                    src={avatar.Avatar?avatar.Avatar:"https://material-ui.com/static/images/avatar/1.jpg"}
+                    alt={avatar.avatar.toString()}
+                    src={avatar.avatar?avatar.avatar:"https://material-ui.com/static/images/avatar/1.jpg"}
                     style={{ width: '20vw', height: '20vw', margin: '5vw' }}
                   ></Avatar>
                 </Link>
               );
             })}
-            {users.map((avatar) => {
-              return (
-                <Link
-                  to={`/User/${
-                    avatar.Userid ? avatar.Userid : avatar.toString()
-                  }`}
-                  key={avatar.Userid ? avatar.Userid : avatar.toString()}
-                >
-                  <Avatar
-                    alt={avatar.toString()}
-                    src={avatar.Avatar?avatar.Avatar:"https://material-ui.com/static/images/avatar/1.jpg"}
-                    style={{ width: '20vw', height: '20vw', margin: '5vw' }}
-                  ></Avatar>
-                </Link>
-              );
-            })}
+            
           </div>
         ) : (
           <div>
