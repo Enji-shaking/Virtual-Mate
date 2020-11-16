dbcbe2348b9a7005c6fa26011a5ed06592e38f33
Enji-shaking <enjili@usc.edu>
2020-11-16 09:59:22 +0800

    Update imageController.java

--- back-end/myArtifact/src/main/java/com/virtualmate/myArtifact/api/imageController.java
+++ back-end/myArtifact/src/main/java/com/virtualmate/myArtifact/api/imageController.java
@@ -1,6 +1,35 @@
 package com.virtualmate.myArtifact.api;
 
+import com.virtualmate.myArtifact.model.Image;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/image")
+@RestController
 public class imageController {
-    create image
-    get image by id
+    public final ImageService imageService;
+    @Autowired
+	public imageController(ImageService imageService) {
+		this.imageService = imageService;
+    }
+    
+    @GetMapping("{imageId}")
+    public Image getImageById(@PathVariable String imageId) {
+		return imageService.getImageById(imageId);
+    }
+    /*
+        imageUrl: "xxxx.xxxx.jpg"
+    */
+    @PostMapping("create")
+    public Image createImage(@RequestBody Image image){
+        imageService.addImage(image)l
+        return image;
+    }
+
 }
