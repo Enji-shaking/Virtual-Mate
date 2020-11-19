194d92348b5d5837a4c2092e57dc10c02a2c2ad7
Tigojiang <tigojian@usc.edu>
2020-11-19 15:15:08 +0800

    imgae cors

--- back-end/myArtifact/src/main/java/com/virtualmate/myArtifact/api/imageController.java
+++ back-end/myArtifact/src/main/java/com/virtualmate/myArtifact/api/imageController.java
@@ -4,14 +4,10 @@ import com.virtualmate.myArtifact.model.Image;
 
 import com.virtualmate.myArtifact.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("api/image")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class imageController {
     public final ImageService imageService;
